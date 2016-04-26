d0d491b7ec154e199c792b64d416778ab4f085a8
Alexey Trotskovets <trotskovets@gmail.com>
2016-04-26 18:42:08 +0300

    add hello service module

--- src/community/hello/src/main/java/HelloWorld.java
+++ src/community/hello/src/main/java/HelloWorld.java
@@ -1,26 +1,16 @@
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.vfny.geoserver.ExceptionHandler;
-import org.vfny.geoserver.servlets.AbstractService;
+public class HelloWorld {
 
-public class HelloWorld extends AbstractService {
+  public HelloWorld() {
+    // Do nothing
+  }
 
-	public HelloWorld() {
-		super( "HWS", "SayHello", null );
-	}
-
-	protected ExceptionHandler getExceptionHandler() {
-		return null;
-	}
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) 
-		throws ServletException, IOException {
-		
-		response.getOutputStream().write( "Hello World".getBytes() );
-	}
-	
+  public void sayHello(HttpServletRequest request, HttpServletResponse response)
+  throws ServletException, IOException {
+    response.getOutputStream().write( "Hello World".getBytes() );
+  }
 }
