f9bd8cf4ed216258d1f3f3f56e3d175e062f50f4
Aleksei Trotskovets <trotskovets@gmail.com>
2016-05-13 12:13:37 +0300

    try with resources refactoring

--- src/community/wms-leaflet/src/main/java/LeafletResourceLoader.java
+++ src/community/wms-leaflet/src/main/java/LeafletResourceLoader.java
@@ -31,48 +31,41 @@ public class LeafletResourceLoader {
 		ServletContext context = request.getSession().getServletContext();
 		String path = request.getParameter("path");
 		ClassLoader classLoader = LeafletResourceLoader.class.getClassLoader();
-		InputStream inputStream = null;
-		OutputStream outputStream = null;
-		try {
+		
+		try (	InputStream inputStream = classLoader.getResourceAsStream(path);
+				OutputStream outputStream = response.getOutputStream()) {
 			String mimeType = context.getMimeType(classLoader.getResource(path).toURI().toString());
 			if (mimeType == null) {
 				mimeType = "text/plain";
 			}
 			response.setContentType(mimeType);
-			inputStream = classLoader.getResourceAsStream(path);			
-			outputStream = response.getOutputStream();
 			byte[] buffer = new byte[4096];
 			int bytesRead = -1;
-			
 			while ((bytesRead = inputStream.read(buffer)) != -1) {
 				outputStream.write(buffer, 0, bytesRead);
 			}
-		} catch (FileNotFoundException e) {
-			response.getWriter().write(TEXT_RESPONSE_FILE_NOT_FOUND + e);
 		} catch (IOException e) {
-			response.getWriter().write(TEXT_RESPONSE_FILE_NOT_FOUND + e);
-		} finally {
-			if (inputStream != null) {
-				inputStream.close();
-			}
-			if (outputStream != null) {
-				outputStream.close();
-			}
+			e.printStackTrace();
 		}
 	}
 
-	public void getSource(HttpServletRequest request, HttpServletResponse response) {
+	public void getSource(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, URISyntaxException {
 		String layerName = request.getParameter("layer");
 		String dataDir = geoServerDataDir.root().getAbsolutePath();
 		String coverageId = catalog.getLayerByName(layerName).getResource().getId();
 		String coverageStoreId = catalog.getCoverage(coverageId).getStore().getId();
 		String pathFromXml = catalog.getCoverageStore(coverageStoreId).getURL();
 		
-		FileInputStream inputStrem = null;
-		OutputStream outputStream = null;
+		File downloadFile = null;
 		try {
-			File downloadFile = getFile(dataDir, pathFromXml);
-			inputStrem = new FileInputStream(downloadFile);
+			downloadFile = getFile(dataDir, pathFromXml);
+		} catch (FileNotFoundException e) {
+			response.getWriter().write(TEXT_RESPONSE_FILE_NOT_FOUND + e);
+		}
+		
+		try (	FileInputStream inputStream = new FileInputStream(downloadFile);
+				OutputStream outputStream = response.getOutputStream()) {
 			response.setContentType("application/octet-stream");
 			response.setContentLength((int) downloadFile.length());
 
@@ -80,39 +73,18 @@ public class LeafletResourceLoader {
 			String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
 			response.setHeader(headerKey, headerValue);
 
-			outputStream = response.getOutputStream();
-
 			byte[] buffer = new byte[4096];
 			int bytesRead = -1;
 
-			while ((bytesRead = inputStrem.read(buffer)) != -1) {
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
 				outputStream.write(buffer, 0, bytesRead);
 			}
-		} catch (FileNotFoundException e) {
-			
-		} catch (IOException e) {
-			
-		} finally {
-			if (inputStrem != null) {
-				try {
-					inputStrem.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			if (outputStream != null) {
-				try {
-					outputStream.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+		}catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public File getFile(String geoServerDataDir, String fileFromXml) throws FileNotFoundException {
+	private File getFile(String geoServerDataDir, String fileFromXml) throws FileNotFoundException {
 		
 		// Replace path divider from "\" to "/" if need
 		geoServerDataDir = geoServerDataDir.replace("\\", "/");
@@ -137,7 +109,6 @@ public class LeafletResourceLoader {
 					" 2nd) tried use this location: " + fullPath;
 			throw new FileNotFoundException(e);
 		}
-		
 		return file;
 	}
 }
