d8fdc8c429afc4ce262377153a5992127cfd31c7
Aleksei Trotskovets <trotskovets@gmail.com>
2016-05-13 12:41:29 +0300

    skeleton for source-downloader plagin was added

---
+++ src/community/source-downloader/src/main/java/SourceDownloader.java
@@ -0,0 +1,91 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.config.GeoServerDataDirectory;
+
+public class SourceDownloader {
+	
+	private Catalog catalog;
+	private GeoServerDataDirectory geoServerDataDir;
+
+	private static final String TEXT_RESPONSE_FILE_NOT_FOUND = "Looks like server can't find specified file.";
+
+	public SourceDownloader(Catalog catalog, GeoServerDataDirectory geoServerDataDir) {
+		this.catalog = catalog;
+		this.geoServerDataDir = geoServerDataDir;
+	}
+
+	public void downloadSourceFile(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, URISyntaxException {
+		String layerName = request.getParameter("layer");
+		String dataDir = geoServerDataDir.root().getAbsolutePath();
+		String coverageId = catalog.getLayerByName(layerName).getResource().getId();
+		String coverageStoreId = catalog.getCoverage(coverageId).getStore().getId();
+		String pathFromXml = catalog.getCoverageStore(coverageStoreId).getURL();
+		
+		File downloadFile = null;
+		try {
+			downloadFile = getFile(dataDir, pathFromXml);
+		} catch (FileNotFoundException e) {
+			response.getWriter().write(TEXT_RESPONSE_FILE_NOT_FOUND + e);
+		}
+		
+		try (	FileInputStream inputStream = new FileInputStream(downloadFile);
+				OutputStream outputStream = response.getOutputStream()) {
+			response.setContentType("application/octet-stream");
+			response.setContentLength((int) downloadFile.length());
+
+			String headerKey = "Content-Disposition";
+			String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+			response.setHeader(headerKey, headerValue);
+
+			byte[] buffer = new byte[4096];
+			int bytesRead = -1;
+
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesRead);
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private File getFile(String geoServerDataDir, String fileFromXml) throws FileNotFoundException {
+		
+		// Replace path divider from "\" to "/" if need
+		geoServerDataDir = geoServerDataDir.replace("\\", "/");
+		fileFromXml = fileFromXml.replace("\\", "/");
+		
+		// Trim word "file://" or "file:" from getting URL
+		if (fileFromXml.startsWith("file://")) {
+			fileFromXml = fileFromXml.substring(7);
+		} else {
+			fileFromXml = fileFromXml.substring(5);
+		}
+		
+		// Set absolute path location
+		String fullPath = geoServerDataDir + "/" + fileFromXml;
+						
+		File file = new File(fullPath);
+		if(!file.exists()) {
+			file = new File(fileFromXml);
+		}
+		if (!file.exists()) {
+			String e = "1st) tried use this location: " + fileFromXml +
+					" 2nd) tried use this location: " + fullPath;
+			throw new FileNotFoundException(e);
+		}
+		return file;
+	}
+}
