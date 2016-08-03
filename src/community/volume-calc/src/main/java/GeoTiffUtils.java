ee32b76e90e379ecf13e98e1238c5314e10e917c
Aleksei Trotskovets <trotskovets@gmail.com>
2016-08-03 13:25:45 +0300

    add new methods convertCoordinates and isCoordinatesInTheBorder.

--- src/community/volume-calc/src/main/java/GeoTiffUtils.java
+++ src/community/volume-calc/src/main/java/GeoTiffUtils.java
@@ -3,10 +3,21 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.batik.svggen.font.table.Coverage;
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.geometry.Envelope2D;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
+
 import com.vividsolutions.jts.geom.Coordinate;
 
 public class GeoTiffUtils {
 	private static final String REG_EXP = "([0-9]*\\.[0-9]*),([0-9]*\\.[0-9]*)";
+	private static final String SOURCE_CRS = "EPSG:4326";
 	
 	public static Coordinate[] extractCoordinates(String geoJSON) {
 		List<Coordinate> listOfCoordinates = new ArrayList<>();		
@@ -22,6 +33,61 @@ public class GeoTiffUtils {
 		Coordinate[] coords  = new Coordinate[listOfCoordinates.size()];
 		coords = listOfCoordinates.toArray(coords);
 		return coords;
+	}
+	
+	public static CoordinateReferenceSystem getTargetCRS(GridCoverage2D coverage) {
+		return coverage.getCoordinateReferenceSystem();
+	}
+	
+	public static Coordinate[] convertCoordinates(GridCoverage2D coverage, Coordinate[] coords) throws TransformException, FactoryException {
+		CoordinateReferenceSystem sourceCRS = CRS.decode(SOURCE_CRS);
+		CoordinateReferenceSystem targetCRS = getTargetCRS(coverage);
+		MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS);
+		System.out.println("Transormation of the coordinates:");
+		for (int i = 0; i < coords.length; i++) {
+			System.out.print("Was: " + coords[i] + " / ");
+			JTS.transform(coords[i], coords[i], transform);
+			System.out.println("Become: " + coords[i]);	
+		}
+		System.out.println("End of transformation");
+		return coords;
+	}
+	
+	public static boolean isCoordinatesInTheBorder(Coordinate[] coords, GridCoverage2D coverage) {
+		Envelope2D envelope = coverage.getEnvelope2D();
+		CoordinateReferenceSystem crs = envelope.getCoordinateReferenceSystem();
+		System.out.println("Envelope - " + crs.getName());
+		for (Coordinate coordinate : coords) {
+			if (!envelope.contains(coordinate.x, coordinate.y)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static Coordinate[] getExtrimeCorners(Coordinate[] coords) {
+		Coordinate firstCorner = coords[0];
+		Coordinate secondCorner = coords[0];
+		for (Coordinate coordinate : coords) {
+			if (coordinate.x < firstCorner.x) {
+				firstCorner.x = coordinate.x;
+			}
+			if (coordinate.y > firstCorner.y) {
+				firstCorner.y = coordinate.y;			
+			}
+			if (coordinate.x > secondCorner.x) {
+				secondCorner.x = coordinate.x;
+			}
+			if (coordinate.y < secondCorner.y) {
+				secondCorner.y = coordinate.y;
+			}
+		}
+		Coordinate[] res = {firstCorner, secondCorner};
+		System.out.println("Extrime corner is:");
+		System.out.println("First Corner = " + firstCorner.x + " / " + firstCorner.y);
+		System.out.println("Second Corner = " + secondCorner.x + " / " + secondCorner.y);
+		return res;
+		
 	}
 	
 	public static double getAbsDimOfPixel () {
--- src/community/volume-calc/src/main/java/VolumeCalculator.java
+++ src/community/volume-calc/src/main/java/VolumeCalculator.java
@@ -7,7 +7,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.config.GeoServerDataDirectory;
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverage.grid.io.GridCoverage2DReader;
+import org.geotools.gce.geotiff.GeoTiffReader;
 import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.opengis.coverage.grid.GridEnvelope;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
+
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Polygon;
@@ -26,7 +33,7 @@ public class VolumeCalculator {
 	}
 
 	public void sendVolume(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException, URISyntaxException {
+			throws ServletException, IOException, URISyntaxException, TransformException, FactoryException {
 		String layerName = request.getParameter("layer");
 		String geoJSON = request.getParameter("geoData");
 		String dataDir = geoServerDataDir.root().getAbsolutePath();
@@ -34,9 +41,38 @@ public class VolumeCalculator {
 		String coverageStoreId = catalog.getCoverage(coverageId).getStore().getId();
 		String pathFromXml = catalog.getCoverageStore(coverageStoreId).getURL();
 		
+		/*Получаем файл, получаем необходимую информацию о GeoTIFF*/		
+		GridCoverage2DReader reader = new GeoTiffReader(getFile(dataDir, pathFromXml));
+		GridEnvelope dimensions = reader.getOriginalGridRange();
+		GridCoverage2D coverage = reader.read(null);
+		
+		/*Обрабатываем geoJson, получаем координаты, создаем по координатам полигон*/
 		Coordinate[] coords = GeoTiffUtils.extractCoordinates(geoJSON);
+		coords = GeoTiffUtils.convertCoordinates(coverage, coords);
+		Coordinate[] extrimeCorners = GeoTiffUtils.getExtrimeCorners(coords);
 		GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
 		Polygon polygon = geometryFactory.createPolygon(coords);
+		
+		
+		
+		/*Проверим попадает ли полигон на картинку геоТиффа*/
+		if(GeoTiffUtils.isCoordinatesInTheBorder(coords, coverage)) {
+			response.getWriter().write("Inside poligon");
+		} else {
+			response.getWriter().write("Outside polygon");
+		}
+		
+		
+		
+		
+		
+		/*Получаем размер пикселя*/
+		
+		
+	
+		
+		/*Получаем размер пикселя - ОКОНЧАНИЕ*/
+		
 		
 		File downloadFile = null;
 		try {
